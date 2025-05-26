1cc82c41bc1bbdf689285a023337a61db1cf46ee
Densodim <25denso@gmail.com>
2025-05-26 19:51:07 +0300

    modified server add cors

--- server/index.ts
+++ server/index.ts
@@ -24,6 +24,14 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
